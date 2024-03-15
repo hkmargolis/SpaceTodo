5e81324b482184e9a1bf468a9ce51e19d16e27eb
hkmargolis <100984815+hkmargolis@users.noreply.github.com>
2024-03-15 10:12:54 -0700

    TaskPanel.java

--- TaskPanel.java
+++ TaskPanel.java
@@ -1,19 +1,20 @@
 package space.todoapp;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.GridLayout;
-import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 /**
+ * This class creates a TaskPanel object.
  *
  * @author Hannah
  */
 public class TaskPanel extends JPanel {
 
+    //constructor
     public TaskPanel() {
+        //set up TaskPanel JPanel
         GridLayout Layout = new GridLayout(10, 1);
         Layout.setVgap(5);
         this.setLayout(Layout);
